89c5854489a542c7af8f743d7944597f504ac341
achmadqusyairii <achmadqusayiri291017@gmail.com>
2022-10-15 11:17:11 +0700

    point_siswa

--- application/views/tamplate/sidebar.php
+++ application/views/tamplate/sidebar.php
@@ -1,49 +1,47 @@
         <!-- Sidebar -->
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-<!-- Sidebar - Brand -->
-<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-    <div class="sidebar-brand-icon rotate-n-15">
-        <i class=""></i>
-    </div>
-    <!-- <img class="" src="<?php echo base_url()?>assets/img/logo.png"> -->
-    <div class="sidebar-brand-text mx-3">IBUK-SIPA <sup></sup></div>
-</a>
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class=""></i>
+                </div>
+                <!-- <img class="" src="<?php echo base_url() ?>assets/img/logo.png"> -->
+                <div class="sidebar-brand-text mx-3">IBUK-SIPA <sup></sup></div>
+            </a>
 
-<!-- Divider -->
-<hr class="sidebar-divider my-0">
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
 
-<!-- Nav Item - Dashboard -->
-<li class="nav-item active">
-    <a class="nav-link" href="http://localhost/ibuk-sipa/">
-        <i class=""></i>
-        <span>Dashboard</span></a>
-</li>
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="http://localhost/ibuk-sipa/">
+                    <i class=""></i>
+                    <span>Dashboard</span></a>
+            </li>
 
-<!-- Divider -->
-<hr class="sidebar-divider">
+            <!-- Divider -->
+            <hr class="sidebar-divider">
 
-<!-- Heading -->
-<div class="sidebar-heading">
-    MASTER DATA
-</div>
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                MASTER DATA
+            </div>
 
-<!-- Nav Item - Pages Collapse Menu -->
+            <!-- Nav Item - Pages Collapse Menu -->
 
-<!-- <li class="nav-item">
+            <!-- <li class="nav-item">
     <a class="nav-link" href="http://localhost/kta-gerindra/level/index/">
        <i class="fas fa-thin fa-address-card"></i>
         <span>LEVEL JABATAN</span></a>
 </li> -->
 
-<li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                    aria-expanded="true" aria-controls="collapseUtilities">
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-thin fa-map"></i>
                     <span>MASTER DATA</span>
                 </a>
-                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                    data-parent="#accordionSidebar">
+                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Referensi</h6>
                         <a class="collapse-item" href="http://[::1]/ibuk-sipa/jabatan/index/">Jabatan</a>
@@ -55,48 +53,56 @@
                     </div>
                 </div>
             </li>
+            <hr class="sidebar-divider">
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fa fa-list-ul"></i>
+                    <span>MODUL B.K</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Bimbingan Konseling:</h6>
+                        <a class="collapse-item" href="buttons.html">Point siswa</a>
+                        <a class="collapse-item" href="cards.html">Laporan siswa</a>
+                    </div>
+                </div>
+            </li>
 
- <!-- <li class="nav-item">
-    <a class="nav-link" href="charts.html">
-        <i class="fas fa-user"></i>
-        <span>ANGGOTA</span></a>
-</li> -->
 
 
 
+            <!-- Divider -->
+            
 
-<!-- Divider -->
-<hr class="sidebar-divider">
-
-<!-- Heading -->
-<!-- <div class="sidebar-heading">
+            <!-- Heading -->
+            <!-- <div class="sidebar-heading">
     DATA ANGGOTA
 </div> -->
 
-<!-- Nav Item - Charts -->
-<!-- <li class="nav-item">
+            <!-- Nav Item - Charts -->
+            <!-- <li class="nav-item">
     <a class="nav-link" href="http://localhost/kta-gerindra/anggota/index/">
         <i class="fas fa-thin fa-plus"></i>
         <span>ANGGOTA</span></a>
 </li> -->
-<!-- Heading -->
-<!-- <div class="sidebar-heading">
+            <!-- Heading -->
+            <!-- <div class="sidebar-heading">
     LAPORAN
 </div> -->
 
-<!-- Nav Item - Charts -->
-<!-- <li class="nav-item">
+            <!-- Nav Item - Charts -->
+            <!-- <li class="nav-item">
     <a class="nav-link" href="http://localhost/kta-gerindra/anggota/printkta/">
         <i class="fas fa-thin fa-file"></i>
         <span>LAPORAN</span></a>
 </li> -->
-<!-- Heading -->
-<!-- <div class="sidebar-heading">
+            <!-- Heading -->
+            <!-- <div class="sidebar-heading">
     USER
 </div> -->
 
-<!-- Nav Item - Charts -->
-<!-- <li class="nav-item">
+            <!-- Nav Item - Charts -->
+            <!-- <li class="nav-item">
     <a class="nav-link" href="charts.html">
         <i class="fas fa-thin fa-file"></i>
         <span>DATA USER</span></a>
@@ -104,5 +110,5 @@
 
 
 
-</ul>
-<!-- End of Sidebar -->
+        </ul>
+        <!-- End of Sidebar -->
