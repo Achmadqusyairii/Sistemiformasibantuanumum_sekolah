ec432600244795e93f92f19980dd74bb33c6b962
achmad <achmadqusayiri291017@gmail.com>
2022-10-20 09:14:01 +0700

    css login

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Data Jamaah | Admin</title>
+  <title>IBUK -SIPA</title>
   <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
@@ -38,7 +38,7 @@
       <div class="card login-card">
         <div class="row no-gutters">
           <div class="col-md-5">
-            <img src="<?php echo site_url('assets/images/hero-bg3.jpg'); ?>" alt="login" class="login-card-img">
+            <img src="<?php echo base_url('assets/images/hero-bg3.jpg'); ?>" alt="login" class="login-card-img">
           </div>
           <div class="col-md-6">
             <div class="card-body">
