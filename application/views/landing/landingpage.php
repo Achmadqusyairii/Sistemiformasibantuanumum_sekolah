a88afbf9e41530e6366559eeeb73d666d01dd31c
achmad <achmadqusayiri291017@gmail.com>
2022-10-27 15:37:38 +0700

    PERBAIKAN INDEX

--- application/views/landing/landingpage.php
+++ application/views/landing/landingpage.php
@@ -46,7 +46,7 @@
       <button type="button" class="nav-toggle"><i class="bx bx-menu"></i></button>
       <nav class="nav-menu">
         <ul>
-          <li class="active"><a href="http://localhost/ibuk-sipa/index.php/login" class="scrollto">Login</a></li>
+          <li class="active"><a href="<?php echo base_url() ?>/login" class="scrollto">Login</a></li>
           <li><a href="#about" class="scrollto">About Us</a></li>
           <li><a href="#why-us" class="scrollto">Why Us</a></li>
           <li class="drop-down"><a href="">Drop Down</a>
